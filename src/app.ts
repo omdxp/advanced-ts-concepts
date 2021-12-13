68bce18e26655ffac1cd23ff1be29ad171d101ae
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-13 11:11:32 +0100

    all combinations as type 😙

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,19 @@ type IndexOf<T extends [], S extends number[]> = T["length"] extends S["length"]
   ? S[number]
   : IndexOf<T, [S["length"], ...S]>;
 
-a.map((_, i: IndexOf<typeof a>) => {
-  const c = a[i + 1];
-}); // [0, 1, 2]))
+// a.map((_, i: IndexOf<typeof a, []>) => {
+//   const c = a[i + 1];
+// }); // [0, 1, 2]))
 
 console.log(typeof a[0]);
+
+type Row = "a" | "b";
+type Col = "1" | 2;
+
+const toPosition = (position: `${Row}${Col}`) => {
+  const [row, col] = position.split("");
+  return { row, col };
+};
+
+const tmp = toPosition("b2");
+console.log(tmp);
