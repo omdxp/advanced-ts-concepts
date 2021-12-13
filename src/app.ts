eb0e1064d5f4ff0ee059434a0b1ddfa073379bf5
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-13 11:06:29 +0100

    index of helper type

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 const a = [1, 2, 3] as const; // tuple
-const b = a[2]!; // error
 
-console.log(typeof b);
+type IndexOf<T extends [], S extends number[]> = T["length"] extends S["length"]
+  ? S[number]
+  : IndexOf<T, [S["length"], ...S]>;
+
+a.map((_, i: IndexOf<typeof a>) => {
+  const c = a[i + 1];
+}); // [0, 1, 2]))
+
+console.log(typeof a[0]);
