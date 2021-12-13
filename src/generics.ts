f71d34e5891d274245565aa0e70d81362104457a
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-13 09:44:01 +0100

    generics

---
+++ src/generics.ts
@@ -0,0 +1,29 @@
+enum TaskType {
+  feature = "feature",
+  bug = "bug",
+}
+
+type Task<T = TaskType> = {
+  name: string;
+  type: T;
+};
+
+const whatever: Task = {
+  name: "whatever",
+  type: TaskType.feature,
+};
+whatever.type = TaskType.bug;
+
+type FeatureTask = Task<TaskType.feature>;
+
+const feature: FeatureTask = {
+  name: "feature",
+  type: TaskType.feature,
+};
+
+feature.type = TaskType.bug;
+
+const invalid: FeatureTask = {
+  name: "invalid",
+  type: TaskType.bug,
+};
